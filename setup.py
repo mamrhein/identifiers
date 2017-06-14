e47d80a0c79bd19f97c891169e2393141483a763
Michael Amrhein <michael@adrhinum.de>
2017-06-14 21:36:35 +0200

    Dropped Python 3.4, added Python 3.5, Python 3.6 and Python Imlementations CPython and PyPy.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     keywords='identifier GS1 GLN GTIN SSCC GSIN ISBN ISMN ISSN BIC IBAN',
     platforms='all',
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
@@ -29,7 +29,10 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython"
+        "Programming Language :: Python :: Implementation :: PyPy"
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules"
