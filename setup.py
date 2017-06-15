bfcb5f824792e74f2c6f8a24b5d30ea7babe7c9f
Michael Amrhein <michael@adrhinum.de>
2017-06-15 20:16:35 +0200

    Fixed missing commas in setup.py.

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: CPython"
-        "Programming Language :: Python :: Implementation :: PyPy"
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules"
